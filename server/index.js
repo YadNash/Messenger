4ecfa9bcf78ce158ee75eaacfd3b7b1af3bcfb7e
Avinash Yadav <109527105+logan-14@users.noreply.github.com>
2024-10-18 00:22:53 +0530

    realtime

---
+++ server/index.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import mongoose from 'mongoose'
+import authRoutes from './routes/AuthRoutes.js'
+import contactRoutes from './routes/ContactRoutes.js'
+import setupSocket from './socket.js'
+import messagesRoutes from './routes/MessagesRoutes.js'
+import channelRoutes from './routes/ChannelRoutes.js'
+
+
+dotenv.config();
+
+const app = express()
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(cors({
+    origin:["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+})
+);
+
+app.use("/uploads/profiles", express.static("uploads/profiles"));
+app.use("/uploads/files",express.static("uploads/files"));
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/contacts', contactRoutes);
+app.use('/api/messages', messagesRoutes);
+app.use("/api/channel", channelRoutes);
+
+const server = app.listen(port, ()=>{
+    console.log(`Server is running at ${port}`)
+})
+
+setupSocket(server);
+
+mongoose.connect(databaseURL)
+.then(()=> console.log("DB Connection Successfull"))
+.catch((err) => console.log(err.message));
